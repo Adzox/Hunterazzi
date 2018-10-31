5967c00e1736ad9ed1753d6fbea79a6ddeee9a9d
Olle Renius <olle_9516@hotmail.com>
2018-10-31 12:33:59 +0100

    Fix folder structure :D :D :D

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour {
+
+    public InfluenceMap map;
+    List<Vector2Int> path;
+    Vector2Int currentPos;
+    Vector2Int nextPos;
+
+    public float speed;
+
+	void Start () {
+        path = new List<Vector2Int>();
+		
+	}
+	
+	void Update () {
+        currentPos = map.grid.WorldToGrid(transform.position);
+        path = InfluenceMapNavigation.FindMax(map, currentPos, 10, 0);
+        if (path.Count > 0) {
+            nextPos = path[0];
+            if (currentPos == nextPos) path.Remove(path[0]);
+        } else {
+            nextPos = currentPos;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, map.grid.GridToWorld(nextPos), speed * Time.deltaTime);
+		
+	}
+}
